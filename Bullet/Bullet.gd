6f171ddd882e671f418a321dceddb910a0232f23
BosstheBIGSHOT <8092007@schoolsnet.act.edu.au>
2022-03-08 15:14:09 +1100

    update 2
    full auto and semi auto added

--- Bullet/Bullet.gd
+++ Bullet/Bullet.gd
@@ -7,10 +7,12 @@ func _ready():
 	set_physics_process(true)
 
 func _physics_process(delta):
-	var collidedObject = move_and_collide(Vector2(0, -speed*delta))
+	var collidedObject = move_and_collide(Vector2(-speed*delta, 0))
 	if (collidedObject):
 		print(collidedObject.collider.name)
 		if "Enemy" in collidedObject.collider.name:
 			collidedObject.get_collider().queue_free()
 			GlobalVariables.scoringInformation["currentScore"] +=10
+		if "Border_Left" in collidedObject.collider.name:
+			collidedObject.get_collider().queue_free()
 		queue_free()
--- Player/Player.gd
+++ Player/Player.gd
@@ -1,6 +1,5 @@
 extends KinematicBody2D
 
-var movement_speed = 420
 var vertical_speed = 10
 var bulletSource = preload ("res://Bullet/Bullet.tscn")
 # Declare member variables here. Examples:
@@ -14,15 +13,15 @@ func _ready():
 	set_physics_process(true)
 	
 func _physics_process(delta):
-	if Input.is_action_pressed("ui_left"):
-		move_and_collide(Vector2(-movement_speed * delta, 0))
-	if Input.is_action_pressed("ui_right"):
-		move_and_collide(Vector2(movement_speed * delta, 0))
 	if Input.is_action_pressed("ui_up"):
 		move_and_collide(Vector2(vertical_speed * delta, -10))
 	if Input.is_action_pressed("ui_down"):
 		move_and_collide(Vector2(vertical_speed * delta, 10))
-	if Input.is_action_just_pressed("fire"):
+	if Input.is_action_pressed("fire"):
+		var bulletInstance = bulletSource.instance()
+		bulletInstance.position = Vector2(position.x, position.y-20)
+		get_tree().get_root().add_child(bulletInstance)
+	if Input.is_action_just_pressed("ui_left"):
 		var bulletInstance = bulletSource.instance()
 		bulletInstance.position = Vector2(position.x, position.y-20)
 		get_tree().get_root().add_child(bulletInstance)
